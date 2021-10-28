db2abbb6ae6adc1e049aa1349e15ca750c2579c7
Michael Medrano <mgmedrano@outlook.com>
2021-10-27 23:19:47 -0700

    Restructure layout, add component contents with placeholders

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "What Are We Having",
+    subtitle: "Collaborative Project",
+    description:
+      "With a team of Student Developers, we utilized 8 weeks of skills (HTML, CSS, JavaScript, and APIs to create an Application what randomizes food and drink based on preference.",
+    image: "./WAWH.Homepage.png",
+    link: "https://imoranmo.github.io/What-are-we-having/",
+  },
+  {
+    title: "Work Day Schedule",
+    subtitle: "Individual Project",
+    description:
+      "Targeted use of HTML, CSS, and JavaScript. Functional daily planner with ability to add important events for effective time management. Enjoyed using Moment technology.",
+    image: "./workDayScheduler.png",
+    link: "https://mgmedrano.github.io/Work_Day_Scheduler/",
+  },
+  {
+    title: "Local4U2U",
+    subtitle: "Collaborative Project",
+    description:
+      "As the world heavily relied on Corporate stores for resources during the Pandemic, it is important more now than ever that we support the community we live in. Support locally!",
+    image: "./local4u2u.png",
+    link: "https://obscure-reef-30952.herokuapp.com/",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "SQL/NoSQL/GraphQL",
+  "NodeJS",
+  "React",
+  "PWA",
+  "MERN",
+];
